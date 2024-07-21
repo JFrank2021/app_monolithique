3770dc7c5e32246124e3f0ba35dfad7e798624d0
JFrank2021 <paouaf@gmail.com>
2024-07-21 12:33:51 +0200

    modif commit de l'application microservice

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, this is a monolithic application running inside a Docker container!"
+    return "Hello, this is a monolithic application running inside a Docker container"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
